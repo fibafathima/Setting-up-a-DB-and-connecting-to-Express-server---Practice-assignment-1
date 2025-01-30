1709c3eb70169d23c77917d9371e6247aff90758
fiba fathima <fibaaminnu@gmail.com>
2025-01-30 09:28:43 +0530

    completed the assignment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const mongo=process.env.mongoURL
 const app = express();
 const port = 3010;
 
@@ -10,6 +15,15 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, async() => {
   console.log(`Example app listening at http://localhost:${port}`);
+  try {
+    await mongoose.connect(mongo)
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    
+  }
 });
+
+
